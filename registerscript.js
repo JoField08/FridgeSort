fc82e677786abf6b09cf09be3559b364084e0572
Jo_field <150937665+JoField08@users.noreply.github.com>
2025-07-01 13:38:46 +0200

    Refactor registerscript.js for improved readability and functionality

--- registerscript.js
+++ registerscript.js
@@ -4,18 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Überprüfung...");
 
     if (!loginData) {
-        console.log("Keine Benutzerdaten gefunden.");
         if (!window.location.href.includes("register.html")) {
-            console.log("Weiterleitung zur Registrierung.");
             window.location.href = "register.html";
         }
         return;
-    } else {
-        console.log("Benutzer bereits registriert.");
     }
 });
 
-function isValidEmail(email) {
+function isValidEmail(email) { //function to validate email format
     let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
@@ -26,7 +22,7 @@ function saveUserInfo() {
     let email = document.getElementById("inputEmail").value.trim();
     let errorMessage = document.getElementById("error-message");
 
-    errorMessage.textContent = ""; // Fehlernachricht zurücksetzen
+    errorMessage.textContent = "";
 
     if (name === "" || lastName === "" || email === "") {
         errorMessage.textContent = "Bitte fülle alle Felder aus, um fortzufahren!";
@@ -41,10 +37,7 @@ function saveUserInfo() {
     }
 
     let userLoginDataArray = JSON.stringify({ name, lastName, email });
-
     localStorage.setItem("userLoginData", userLoginDataArray);
-
     console.log("Gespeicherte Werte:", JSON.parse(localStorage.getItem("userLoginData")));
-
     window.location.href = "profile.html";
 }
